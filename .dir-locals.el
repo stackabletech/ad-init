292938ce6f26d87342ba4ed067ff9ae1962c5ea6
Natalie Klestrup Röijezon <nat@nullable.se>
2024-08-16 21:39:16 +0200

    There was a time before Windows... A happier time...

---
+++ .dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((yaml-mode . ((yaml-indent-offset . 2)
+               (evil-shift-width . 2))))
