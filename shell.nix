0e97063a63d63c60eacd80668def34772419a470
Natalie Klestrup Röijezon <nat@nullable.se>
2024-08-22 01:08:31 +0200

    More NixOS fixes
    - Override UEFI firmware path
    - Provide python-libvirt to Ansible's main environment too

--- shell.nix
+++ shell.nix
@@ -1,15 +1,23 @@
 { sources ? import nix/sources.nix
 , pkgs ? import sources.nixpkgs {} }:
 
-with pkgs;
+let
+  python = pkgs.python3;
+  extraAnsibleDeps = pypkgs: [ pypkgs.libvirt pypkgs.lxml ];
+in
+pkgs.mkShell rec {
+  # buildInputs = [ pkgs.ansible ];
+  buildInputs = [ ansible ];
 
-mkShell {
-  buildInputs = [ pkgs.ansible ];
+  # Ansible runs plugins in its own interpreter, so we need to add plugin dependencies to
+  # its Python environment
+  ansible = python.pkgs.toPythonApplication
+    (python.pkgs.ansible-core.overridePythonAttrs (old: {
+      propagatedBuildInputs = old.propagatedBuildInputs ++ extraAnsibleDeps python.pkgs;
+    }));
 
   # Ansible runs modules in a separate interpreter, which bypasses Nix's added packages
   # Hence, override that interpreter with one that has the required dependencies installed
-  ANSIBLE_PYTHON_INTERPRETER = pkgs.python3.withPackages (pkgs: [
-    # community.libvirt
-    pkgs.libvirt pkgs.lxml
-  ]) + "/bin/python";
+  ansiblePython = pkgs.python3.withPackages extraAnsibleDeps;
+  ANSIBLE_PYTHON_INTERPRETER = ansiblePython + "/bin/python";
 }
